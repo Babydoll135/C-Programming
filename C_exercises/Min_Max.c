1cb5bbc03de7a237ebd8f78affa8e36f06e24e2c
Dolly135 <60414795+Babydoll135@users.noreply.github.com>
2024-07-26 00:01:27 -0500

    Update Min_Max.c

--- C_exercises/Min_Max.c
+++ C_exercises/Min_Max.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+int findMax(int num1, int num2, int *pMax, int *pMin){
+    if(num1>num2){
+        *pMin=num2;
+        *pMax=num1;
+
+    }else{
+        *pMin=num1;
+        *pMax=num2;
+
+    }
+}
+void main(){
+    int a=5,b=7;
+    int max, min;
+    findMinMax(a,b,&max,&min);
+    printf("max between %d %d = %d\n",a,b,max);
+    printf("max between %d %d = %d\n",a,b,min);
+}
 
